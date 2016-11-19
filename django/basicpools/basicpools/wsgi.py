4209e0aff52b236f57855be313752840d0836606
johan <johan.philipsson@hotmail.com>
2016-11-19 11:01:05 +0100

    Created django pools projcet

---
+++ django/basicpools/basicpools/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for basicpools project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basicpools.settings")
+
+application = get_wsgi_application()
