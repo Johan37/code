c957227d140d915c48d42f77bac3a9c4f34dc21c
johan <johan.philipsson@hotmail.com>
2017-01-05 20:41:06 +0100

    Work on Number wizard

--- unity/NumberWizard/Assets/scripts/Numberwizard.cs
+++ unity/NumberWizard/Assets/scripts/Numberwizard.cs
@@ -8,10 +8,26 @@ public class Numberwizard : MonoBehaviour {
 	void Start () {
 		print("Welcome to Number Wizard!");
 		print("Pick a number in your head, don't tell me");
+
+        int max = 1000;
+        int min = 1;
+
+        print("The highest number you can pick " + max);
+        print("The lowest number you can pick " + min);
+
+        print("Is the number higher or lower than 500?");
+        print("Up for yes, down for lower, return for equal");
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            print("Up arrow pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            print("Down arrow pressed");
+        }
 	}
 }
