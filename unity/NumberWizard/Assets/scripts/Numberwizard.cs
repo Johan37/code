df2ee6b543cdc3bcb0f02cfcf093b7a692b98239
johan <johan.philipsson@hotmail.com>
2017-01-07 10:32:37 +0100

    Completed Number wizard command line version

--- unity/NumberWizard/Assets/scripts/Numberwizard.cs
+++ unity/NumberWizard/Assets/scripts/Numberwizard.cs
@@ -3,34 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Numberwizard : MonoBehaviour {
+    int max;
+    int min;
+    int guess;
 
 	// Use this for initialization
 	void Start () {
+        StartGame();
+	}
+	
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        print("==========================");
 		print("Welcome to Number Wizard!");
 		print("Pick a number in your head, don't tell me");
 
-        int max = 1000;
-        int min = 1;
 
         print("The highest number you can pick " + max);
         print("The lowest number you can pick " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + " ?");
         print("Up for yes, down for lower, return for equal");
 
-	}
-	
+        max++;
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        print("Is the number higher or lower than " + guess + " ?");
+        print("Up for yes, down for lower, return for equal");
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            print("Up arrow pressed");
+            min = guess;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            print("Down arrow pressed");
+            max = guess;
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won!");
+            StartGame();
         }
 	}
 }
