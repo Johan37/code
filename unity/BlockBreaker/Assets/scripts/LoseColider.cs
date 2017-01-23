8044660e85d88c70e4ff2a0ea7a72b9a53543ebf
johan <johan.philipsson@hotmail.com>
2017-01-23 20:30:55 +0100

    Add losecolider script to BB

---
+++ unity/BlockBreaker/Assets/scripts/LoseColider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseColider : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D trigger) {
+        print("Trigger");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        print( "Colide");
+    }
+}
