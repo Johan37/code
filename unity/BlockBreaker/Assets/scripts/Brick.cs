b6842891ef0b440fe659ed95e4a1deccf6440249
johan <johan.philipsson@hotmail.com>
2017-02-17 18:51:08 +0100

    Add hit animations and consolidating hits

--- unity/BlockBreaker/Assets/scripts/Brick.cs
+++ unity/BlockBreaker/Assets/scripts/Brick.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
+    public Sprite[] hitSprites;
 
-    public int maxHit;
     private int timesHit;
     private LevelManager levelManager;
 
@@ -20,14 +20,28 @@ public class Brick : MonoBehaviour {
 	}
 
     void OnCollisionEnter2D(Collision2D collision) {
+        bool isBreakable = (this.tag == "Breakable");
+        if (isBreakable) {
+            HandleHits();
+        }
+    }
+    
+    void HandleHits() {
         timesHit++;
+        int maxHit = hitSprites.Length + 1;
         if (timesHit >= maxHit ) {
             Destroy(gameObject);
+        } 
+        else {
+            LoadSprites();
         }
     }
 
-    // TODO Remove when we can actually win
-    void SimulateWin() {
-        levelManager.LoadNextLevel(); 
+    void LoadSprites() {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] ) {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
     }
+
 }
