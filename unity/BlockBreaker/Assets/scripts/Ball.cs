2c674302473926f2ae1de9e5cdd9da2b0c99111e
johan <johan.philipsson@hotmail.com>
2017-02-20 22:10:05 +0100

    Add random to ball BB

--- unity/BlockBreaker/Assets/scripts/Ball.cs
+++ unity/BlockBreaker/Assets/scripts/Ball.cs
@@ -30,7 +30,13 @@ public class Ball : MonoBehaviour {
 	}
 
     void OnCollisionEnter2D(Collision2D collision) {
-        if(hasStarted)
+        Vector2 tweak = new Vector2(
+                Random.Range(0f, 0.2f),
+                Random.Range(0f, 0.2f));
+
+        if(hasStarted) {
             audio.Play();
+            GetComponent<Rigidbody2D>().velocity += tweak;
+            }
     }
 }
