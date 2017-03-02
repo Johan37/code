2a7f734fd06fd835b9b82545ec2d07296578a815
johan <johan.philipsson@hotmail.com>
2017-03-02 08:16:36 +0100

    Enemies takes damage and die

---
+++ unity/LaserDefender/Assets/Entities/Enemy/EnemyShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour {
+
+    public float health = 200f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col) {
+        Projectile missile = col.gameObject.GetComponent<Projectile>();
+        if (missile) {
+            Debug.Log("Ship Hit");
+            health -= missile.GetDamage();
+            missile.Hit();
+            if (health <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
