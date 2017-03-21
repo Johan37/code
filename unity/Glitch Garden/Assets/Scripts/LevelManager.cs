9b72712fffe7008991e44e3400a693aa148251e2
johan <johan.philipsson@hotmail.com>
2017-03-21 22:08:27 +0100

    Create splash and start screen

---
+++ unity/Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start () {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name ) {
+        Debug.Log("Level load rec " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("I want to quit");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
