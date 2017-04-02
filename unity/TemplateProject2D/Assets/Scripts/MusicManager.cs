4b6262b6d5af244131d86a66b3ac4581aa61ea04
johan <johan.philipsson@hotmail.com>
2017-04-02 23:51:45 +0200

    [TP] Add music manager script

---
+++ unity/TemplateProject2D/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicArray;
+
+    private AudioSource audioSource;
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start() {
+    }
+
+    void OnLevelWasLoaded (int level) {
+        AudioClip thisLevelMusic = levelMusicArray[level];
+        Debug.Log("Playing clip: " + level + " " + thisLevelMusic);
+
+        if (thisLevelMusic) { // if ther is some music attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
