998d13240191a102429c126f7f0fff328bc2de98
johan <johan.philipsson@hotmail.com>
2017-04-07 22:32:50 +0200

    [TDE] Create TopDown engine project

---
+++ unity/TopDownEngine/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start () {
+        if (autoLoadNextLevelAfter > 0) {
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    void AutoLoadLevel(string name) {
+    }
+
+    public void LoadLevel(string name ) {
+        Debug.Log("Level load rec " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("I want to quit");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
