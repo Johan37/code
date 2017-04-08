1db6ae9152309333262ea4dbe63a113ab07c8c9c
johan <johan.philipsson@hotmail.com>
2017-04-08 23:20:18 +0200

    [TDE] Work on player movement

--- unity/TopDownEngine/Assets/PlayerController.cs
+++ unity/TopDownEngine/Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,12 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0 ,0);
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        } 
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
 	}
 }
