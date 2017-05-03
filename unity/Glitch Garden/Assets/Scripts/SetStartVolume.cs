cbd416b410bc0517a11187cf4303bd9ef3669805
johan <johan.philipsson@hotmail.com>
2017-05-03 22:40:20 +0200

    [GG] Volume set WIP

---
+++ unity/Glitch Garden/Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+    private MusicManager musicManager;
+
+	void Start () {
+	    musicManager = GameObject.FindObjectOfType<MusicManager>();
+        if (musicManager) {
+            float volume = PlayerPrefsManager.GetMasterVolume();
+            musicManager.SetVolume(volume);
+        } 
+        else {
+            Debug.LogWarning("No music manager found");
+        }
+	}
+}
