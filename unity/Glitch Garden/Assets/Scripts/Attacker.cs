d54fa844639fe8d01fe2f92ee8261143abd89ca1
johan <johan.philipsson@hotmail.com>
2017-05-05 23:50:32 +0200

    [GG] Move attackers with script

---
+++ unity/Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(-1f, 1.5f)] public float walkSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+
+	}
+}
