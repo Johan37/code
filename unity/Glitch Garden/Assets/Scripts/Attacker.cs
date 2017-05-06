291d8f575b85690252b807a2908b3f6a7a0696c1
Johan37 <johan.philipsson@hotmail.com>
2017-05-06 21:39:42 +0200

    [GG] Add triggers to gameobjects

--- unity/Glitch Garden/Assets/Scripts/Attacker.cs
+++ unity/Glitch Garden/Assets/Scripts/Attacker.cs
@@ -7,11 +7,16 @@ public class Attacker : MonoBehaviour {
     [Range(-1f, 1.5f)] public float walkSpeed;
 
 	void Start () {
-		
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
 	}
 	
 	void Update () {
 		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
 
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(name + " triggered");
+    }
 }
