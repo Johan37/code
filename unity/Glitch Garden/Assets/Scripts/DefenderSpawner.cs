23a322bc343dc574af7e584426ff3dafd3567800
Johan37 <johan.philipsson@hotmail.com>
2017-05-27 10:41:28 +0200

    [GG] Spawn defender to grid

--- unity/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
+++ unity/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour {
 
+    Camera myCamera;
+
+    private GameObject defenderParent;
+
 	// Use this for initialization
 	void Start () {
-		
+        myCamera = FindObjectOfType<Camera>();
+
+        defenderParent = GameObject.Find("Defenders");
+        if(!defenderParent) {
+            defenderParent = new GameObject("Defenders");
+        }
+
 	}
 	
 	// Update is called once per frame
@@ -15,6 +25,25 @@ public class DefenderSpawner : MonoBehaviour {
 	}
 
     private void OnMouseDown() {
-        
+        Vector2 pos = SnapToGrid(CalculateWorldPosition());
+        GameObject newDefender = Instantiate(Button.selectedDefender, pos, Quaternion.identity);
+        newDefender.transform.parent = defenderParent.transform;
+
+    }
+
+    private Vector2 CalculateWorldPosition() {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+        return worldPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos) {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
     }
 }
