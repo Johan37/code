f17727e37ea0b6011f73c5327aaeb4615e229883
Johan37 <johan.philipsson@hotmail.com>
2017-05-28 11:45:14 +0200

    [GG] Stars as currency

---
+++ unity/Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class StarDisplay : MonoBehaviour {
+
+    private Text starText;
+    private int stars = 0;
+
+	void Start () {
+        starText = GetComponent<Text>();
+	}
+
+    private void UpdateDisplay() {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount) {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void UseStar(int amount) {
+        stars -= amount;
+        UpdateDisplay();
+    }
+}
