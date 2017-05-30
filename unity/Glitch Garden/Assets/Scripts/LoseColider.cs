6ea25cbe5c0d8d0f83614d8854588eed1d020bfe
Johan37 <johan.philipsson@hotmail.com>
2017-05-30 23:19:44 +0200

    [GG] Lose condition

---
+++ unity/Glitch Garden/Assets/Scripts/LoseColider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseColider : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+	void Start () {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        levelManager.LoadLevel("03b Lose");
+    }
+}
