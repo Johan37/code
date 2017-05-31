42d811398a81b235c9f5695d688e5b73b5a4b186
Johan37 <johan.philipsson@hotmail.com>
2017-05-31 22:05:21 +0200

    [GG] Create win slider and commit missed LoseColider

---
+++ unity/Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+    private Slider slider;
+
+	void Start () {
+        slider = GameObject.FindObjectOfType<Slider>();
+	}
+	
+	void Update () {
+		
+	}
+}
