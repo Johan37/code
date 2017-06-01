4bee199e83283673c48362873fe01f580df7dd56
Johan37 <johan.philipsson@hotmail.com>
2017-06-01 23:06:56 +0200

    [GG] Finish win condition

--- unity/Glitch Garden/Assets/Scripts/GameTimer.cs
+++ unity/Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -4,13 +4,38 @@ using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour {
 
+    public float levelSeconds = 90;
+
     private Slider slider;
+    private AudioSource audioSource;
+    private LevelManager levelManager;
+    private bool isEndOfLevel = false;
+    private GameObject winLabel;
 
 	void Start () {
-        slider = GameObject.FindObjectOfType<Slider>();
+        slider = GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
+        levelManager = FindObjectOfType<LevelManager>();
+
+        winLabel = GameObject.Find("You Won");
+        if (!winLabel) {
+            Debug.LogWarning("No win label");
+        }
+        winLabel.SetActive(false);
 	}
 	
 	void Update () {
-		
+        slider.value = Time.timeSinceLevelLoad / levelSeconds;
+        
+        if (Time.timeSinceLevelLoad >= levelSeconds && !isEndOfLevel) {
+            isEndOfLevel = true;
+            audioSource.Play();
+            winLabel.SetActive(true);
+            Invoke("LoadNextLevel", audioSource.clip.length);
+        }
 	}
+
+    void LoadNextLevel() {
+        levelManager.LoadNextLevel();
+    }
 }
